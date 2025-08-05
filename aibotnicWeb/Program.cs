661dbd87509f8df80b27de44f0b9aef190e8fe2f
aibotnic <sameelanka@gmail.com>
2025-08-05 15:46:03 -0500

    first push to github

---
+++ aibotnicWeb/Program.cs
@@ -0,0 +1,59 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.WriteAsync(@"
+        <html>
+            <head>
+                <style>
+                    body {
+                        color: red;
+                    }
+                    input, button {
+                        color: red;
+                        border: 1px solid red;
+                        background-color: #ffe6e6;
+                    }
+                </style>
+            </head>
+            <body>
+                <form method='post' action='/submit'>
+                    <label for='name'>Enter your name:</label>
+                    <input type='text' id='name' name='name' />
+                    <button type='submit'>Submit</button>
+                </form>
+            </body>
+        </html>
+    ");
+});
+
+app.MapPost("/submit", async context =>
+{
+    var form = await context.Request.ReadFormAsync();
+    var name = form["name"];
+
+    context.Response.ContentType = "text/html";
+    await context.Response.WriteAsync($@"
+        <html>
+            <head>
+                <style>
+                    body {{
+                        color: red;
+                    }}
+                    a {{
+                        color: red;
+                    }}
+                </style>
+            </head>
+            <body>
+                <p>Hello, {name}!</p>
+                <a href='/'>Go back</a>
+            </body>
+        </html>
+    ");
+});
+
+app.Run();
+
